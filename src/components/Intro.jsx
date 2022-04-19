dc437d0a0965538d5687476e44a2bf7ad0ee2e0a
Jack <jack200034@hotmail.com>
2022-04-19 11:54:26 +0100

    Create intro component

---
+++ src/components/Intro.jsx
@@ -0,0 +1,10 @@
+export default function Intro() {
+    return(
+        <div>
+            <p className="tag--Text">{`<body/>`}</p>
+            <div className="">
+                <h1>Hi, I'm Jack, Frontend Developer</h1>
+            </div>
+        </div>
+    )
+} 
