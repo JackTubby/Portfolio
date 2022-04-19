7274526f3ac6a6a4de9da87541cbd1aa99d414a7
Jack <jack200034@hotmail.com>
2022-04-20 00:02:31 +0100

    Add nav for large screens

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './Style Sheets/App.css';
 import './Style Sheets/padding.css';
 import Intro from './components/Intro'
+import NavLrg from './components/NavbarLarge'
 
 function App() {
   return (
     <div className="App">
         <Intro />
+        < NavLrg />
     </div>
   );
 }
