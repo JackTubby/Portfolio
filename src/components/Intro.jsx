5618747ea4b43bcbe2ba8b720e48d849ccdfc6a0
Jack <jack200034@hotmail.com>
2022-04-19 13:35:54 +0100

    Add styling classes/tags & get in touch btn

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,9 +1,17 @@
 export default function Intro() {
     return(
         <div>
-            <p className="tag--Text">{`<body/>`}</p>
-            <div className="">
-                <h1>Hi, I'm Jack, Frontend Developer</h1>
+            <p className="intro--tagText">{`<Html/>`}</p>
+            <p className="intro--tagText indent-1">{`<body/>`}</p>
+            <div className="intro--title">
+                <div className="intro--tagText">{`<h1>`}</div>
+                <h1 className="intro--h1">
+                    <div>Hi,</div> 
+                    <div>I'm Jack,</div> 
+                    <div>Frontend Developer</div>
+                </h1>
+                <div className="intro--tagText">{`</h1>`}</div>
+                <button class="animated-border-button">Get In Touch</button>
             </div>
         </div>
     )
