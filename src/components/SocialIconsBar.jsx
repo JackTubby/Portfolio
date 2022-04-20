ef87c1580293fc51c98f06aef3a312e0a107f9cf
Jack <jack200034@hotmail.com>
2022-04-20 17:01:21 +0100

    Create Social Icons Bar Component

---
+++ src/components/SocialIconsBar.jsx
@@ -0,0 +1,16 @@
+import '../Style Sheets/intro.css'
+import gmail from '../images/gmail.png'
+import linkedIn from '../images/linkedIn.png'
+import twitter from '../images/twitter.png'
+
+export default function SocialIconsBar() {
+    return(
+        <div>
+            <div>
+                <img className="intro--socialIcons" src={gmail} alt="gmail icon" />
+                <img className="intro--socialIcons" src={linkedIn} alt="linkedIn icon" />
+                <img className="intro--socialIcons" src={twitter} alt="Twitter Icon" />
+            </div>
+        </div>
+    )
+}
