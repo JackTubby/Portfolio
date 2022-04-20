d2e7cf01a874524b4af0096b7c9952069ada7eca
Jack <jack200034@hotmail.com>
2022-04-20 15:10:16 +0100

    Change .js to .jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,13 @@ import NavLrg from './components/NavbarLarge'
 function App() {
   return (
     <div className="App">
+      <section className='section1'>
         <Intro />
         < NavLrg />
+      </section>
+      <section className='section2'>
+        <h1>Test</h1>
+      </section>
     </div>
   );
 }
