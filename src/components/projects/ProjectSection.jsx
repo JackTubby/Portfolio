75288de92b7b57560e57b980a9a98ec9e08609c7
Jack <jack200034@hotmail.com>
2022-05-05 18:28:51 +0100

    Add project card component

---
+++ src/components/projects/ProjectSection.jsx
@@ -0,0 +1,16 @@
+import ProjectCard from "./ProjectCards"
+
+export default function ProjectSection(){
+
+    
+
+    return (
+        <div className='container'>
+            <div className='row'>
+                <div className='col-3'>
+                    <ProjectCard />
+                </div>
+            </div>
+        </div>
+    )
+}
