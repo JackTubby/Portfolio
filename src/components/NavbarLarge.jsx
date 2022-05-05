2d672998fa7965c692b839874bf05e909a8e66a9
Jack <jack200034@hotmail.com>
2022-05-05 18:28:05 +0100

    Remove code

--- src/components/NavbarLarge.jsx
+++ src/components/NavbarLarge.jsx
@@ -1,14 +1,17 @@
+import React from 'react'
 import '../Style Sheets/nav.css'
 
 export default function NavbarLarge() {
+
     return (
         <nav>
-            <div className="navlrg--wrapper">
+            {/* <button onClick={displayNav}>-</button> */}
+            {/* <div className="navlrg--wrapper">
                 <button className="navlrg--btn">Intro</button>
                 <button className="navlrg--btn">About me</button>
                 <button className="navlrg--btn">My Projects</button>
                 <button className="navlrg--btn">Contact me</button>
-            </div>
+            </div> */}
         </nav>
     )
 }
