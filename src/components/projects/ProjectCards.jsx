c0f3183edbc96c9143fbcfd97d5231a3e29e9d86
Jack <jack200034@hotmail.com>
2022-05-12 19:29:50 +0100

    Add project card content/styling

--- src/components/projects/ProjectCards.jsx
+++ src/components/projects/ProjectCards.jsx
@@ -1,19 +1,27 @@
 import '../../Style Sheets/projects.css'
 import purity from '../../images/purity.png'
 
+import html from './projectLanguageIcons/html.png'
+import css from './projectLanguageIcons/css.png'
+import js from './projectLanguageIcons/js.png'
+import react from './projectLanguageIcons/react.png'
+
 export default function ProjectCards(props) {
     return (
-        <div className='container'>
-            <div className='row'>
-                <div className='col-3'>
-                    <div class="card">
-                        <img src={purity} class="card-img-top" alt="..."></img>
-                        <div class="card-body text-center">
-                            <h5 class="card-title">Purity</h5>
-                            <p class="card-text">This is sample text</p>
-                            <a href="#" class="btn btn-primary">Link</a>
-                        </div>
-                    </div>
+        <div className='card--wrapper'>
+            <img className='card--logo' src={purity} alt="Project Logo" />
+            <div className='card--body'>
+                <h3 className='card--text card--title'>{props.name}</h3>
+                <p className='card--text'>{props.info}</p>
+                <div className='card--text'>
+                    <span><img className='card--icons' src={html} alt="project languages icons" /></span>
+                    <span><img className='card--icons' src={css} alt="project languages icons" /></span>
+                    <span><img className='card--icons' src={js} alt="project languages icons" /></span>
+                    <span><img className='card--icons' src={react} alt="project languages icons" /></span>
+                </div>
+                <div className='card--text'>
+                    <span><button className='card--btn card--message'>Message</button></span>
+                    <span><button className='card--btn card--github'>Visit</button></span>
                 </div>
             </div>
         </div>
