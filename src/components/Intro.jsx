93ce3b817ba1b500a6251f14d3ceb0a14d7b9569
Jack <jack200034@hotmail.com>
2022-05-20 19:17:06 +0100

    Fix classNames to be more universal

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,16 +4,16 @@ import SocialIconsBar from './SocialIconsBar'
 export default function Intro() {
     return(
         <div>
-            <p className="intro--tagText">{`<Html/>`}</p>
-            <p className="intro--tagText indent-1">{`<body/>`}</p>
+            <p className="tagText">{`<Html/>`}</p>
+            <p className="tagText indent-1">{`<body/>`}</p>
             <div className="intro--title">
-                <div className="intro--tagText">{`<h1>`}</div>
-                <h1 className="intro--h1">
+                <div className="tagText">{`<h1>`}</div>
+                <h1 className="intro--h1 headers">
                     <div>Hi,</div> 
                     <div>I'm Jack,</div> 
                     <div>Frontend Developer</div>
                 </h1>
-                <div className="intro--tagText">{`</h1>`}</div>
+                <div className="tagText">{`</h1>`}</div>
                 <button className="animated-border-button">Get In Touch</button>
             </div>
             <div className="intro--socialWrapper">
