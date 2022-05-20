658d0dbd97445b47552cdb4096a1af7d5991ec09
Jack <jack200034@hotmail.com>
2022-05-20 22:20:57 +0100

    Added ugly .alert..

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+alert("Hello you are about to enter Jack's Portfolio\nThe project is still very early in development but i'd love for you to see the progress\nAlso I hate .alert() to ;)");
+
 root.render(
   <React.StrictMode>
     <App />
