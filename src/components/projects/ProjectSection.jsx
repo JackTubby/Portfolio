9f770b890b41ae8f6c912a30659758c57fdaeb68
Jack <jack200034@hotmail.com>
2022-05-20 22:21:32 +0100

    Add project section intro para

--- src/components/projects/ProjectSection.jsx
+++ src/components/projects/ProjectSection.jsx
@@ -23,6 +23,12 @@ export default function ProjectSection(){
                 <div className="tagText">{`<h1>`}</div>
                     <h1 className="headers">My Projects.</h1>
                 <div className="tagText">{`<h1>`}</div>
+                <div className='tagText'>{`<p>`}</div>
+                    <p className='headers--p'>
+                        Check out a couple of my personal favourite projects that I 
+                        worked on. Visit all my projects by clicking 'See More'.
+                    </p>
+                <div className='tagText'>{`</p>`}</div>
             </div>
 
             <div className='container'>
