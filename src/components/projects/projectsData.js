4eddc7b54498c40cf4ca3f5eaa6ba3fc1e5573df
Jack <jack200034@hotmail.com>
2022-05-20 22:21:12 +0100

    Add temp project data

--- src/components/projects/projectsData.js
+++ src/components/projects/projectsData.js
@@ -1,7 +1,12 @@
 export default [
     {
-            'id': 1,
-            'name': 'Purity',
-            'info': 'This is sample Text',
+            id: 1,
+            name: 'Purity',
+            info: 'This is sample Text',
+    },
+    {
+        id: 2,
+        name: 'Test Name',
+        info: 'This is test text'
     }
 ]
